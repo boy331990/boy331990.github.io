48d8c21a7b999b579d99d95e56ab7d10a3fad79e
DESKTOP-6K3H11F\shin <boy331990@naver.com>
2022-04-06 16:40:46 +0900

    gh-pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ module.exports = {
 	reactStrictMode: false,
 	images: {
 		domains: ["avatars.githubusercontent.com"],
-	},
-	assetPrefix:
-    process.env.NODE_ENV === "production" ? "https://boy331990.github.io/portfolio" : ""
+	}
 };
