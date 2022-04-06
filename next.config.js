0e0733527f46b09ba62e6aca47c69a22419dcbbd
DESKTOP-6K3H11F\shin <boy331990@naver.com>
2022-04-06 17:32:13 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
 	reactStrictMode: false,
 	images: {
 		domains: ["avatars.githubusercontent.com"],
-	}
+	},
+	assetPrefix:
+    	process.env.NODE_ENV === "production" ? "https://boy331990.github.io" : ""
 };
